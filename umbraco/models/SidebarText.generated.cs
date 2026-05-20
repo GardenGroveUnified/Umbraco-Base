7626bb00dc2fc1f8967fa4dabd444c2a5fde970a
shivba28 <shivba28@gmail.com>
2026-05-20 12:39:25 -0700

    Refactor Sidebar and Text Content Types
    - Removed the existing 'text' content type configuration and replaced it with a new 'sidebarText' content type.
    - Deleted the generated model for 'Text' and added a new generated model for 'SidebarText'.
    - Created new views for sidebar widgets, including 'desktopSidebar.cshtml' and 'sideTopBar.cshtml', to handle sidebar rendering.
    - Updated media configurations for various accessories, including images and folders, to ensure proper structure and references.
    - Introduced a new media configuration for 'Desk Scanners' with a rename change.

--- umbraco/models/SidebarText.generated.cs
+++ umbraco/models/SidebarText.generated.cs
@@ -18,14 +18,14 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.Common.PublishedModels
 {
-	/// <summary>Text</summary>
-	[PublishedModel("text")]
-	public partial class Text : PublishedElementModel, IBaseAdvancedSettings, IBaseAppearanceSettings, IBaseContent
+	/// <summary>Sidebar Text</summary>
+	[PublishedModel("sidebarText")]
+	public partial class SidebarText : PublishedElementModel, IBaseAdvancedSettings, IBaseAppearanceSettings, IBaseContent
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "17.3.5+1486121")]
-		public new const string ModelTypeAlias = "text";
+		public new const string ModelTypeAlias = "sidebarText";
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "17.3.5+1486121")]
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "17.3.5+1486121")]
@@ -34,14 +34,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 			=> PublishedModelUtility.GetModelContentType(contentTypeCache, ModelItemType, ModelTypeAlias);
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "17.3.5+1486121")]
 		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedContentTypeCache contentTypeCache, Expression<Func<Text, TValue>> selector)
+		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedContentTypeCache contentTypeCache, Expression<Func<SidebarText, TValue>> selector)
 			=> PublishedModelUtility.GetModelPropertyType(GetModelContentType(contentTypeCache), selector);
 #pragma warning restore 0109
 
 		private IPublishedValueFallback _publishedValueFallback;
 
 		// ctor
-		public Text(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
+		public SidebarText(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
 			: base(content, publishedValueFallback)
 		{
 			_publishedValueFallback = publishedValueFallback;
